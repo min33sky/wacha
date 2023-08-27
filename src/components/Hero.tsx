fd63bd129eb30e60a705eb7f78e98d1c811ed0a1
min33sky <min33sky@naver.com>
2023-08-27 17:21:22 +0900

    feat: add variants to framer

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,20 +3,118 @@
 import React from 'react';
 import { useSearchContext } from '@/contexts/SearchContext';
 import Search from './Search';
+import { Button } from './ui/button';
+import Image from 'next/image';
+import { easeInOut, motion } from 'framer-motion';
+import { fadeIn } from '@/lib/framer/variants';
 
 export default function Hero() {
   const { searchActive } = useSearchContext();
 
   return (
     <section className="h-screen xl:h-[90vh] bg-[#b2b7c2]/10" id="home">
-      <div className="container mx-auto h-full xl:pt-10">Hero Container</div>
-      {searchActive ? (
-        <div className="fixed top-[80px] z-10 w-full max-w-[1920px]">
-          <Search />
+      <div className="container mx-auto h-full xl:pt-10">
+        <div className="flex flex-col xl:flex-row justify-center items-center xl:justify-start h-full">
+          <div className="text-center xl:max-w-xl xl:text-left mt-16 xl:mt-0">
+            <motion.h1
+              variants={fadeIn('down', 0.2)}
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{
+                once: false,
+                amount: 0.6,
+              }}
+              className="text-2xl md:text-4xl xl:text-5xl font-bold mb-4 xl:mb-6"
+            >
+              Explore the Finest <span className="text-red-500">Global</span>{' '}
+              Offers
+            </motion.h1>
+            <motion.p
+              variants={fadeIn('down', 0.4)}
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{
+                once: false,
+                amount: 0.6,
+              }}
+              className="max-w-[550px] mx-auto xl:mx-0 mb-6 xl:mb-10"
+            >
+              Find your ideal ride for any adventure with our diverse range of
+              affordable and dependable car rentals.
+            </motion.p>
+
+            <motion.div
+              variants={fadeIn('down', 0.6)}
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{
+                once: false,
+                amount: 0.8,
+              }}
+              className="flex gap-x-3 justify-center xl:justify-start"
+            >
+              <Button variant={'ghost'}>
+                <Image
+                  src="/icons/buttons/app-store.svg"
+                  width={132}
+                  height={36}
+                  alt="appstore"
+                />
+              </Button>
+              <Button variant={'ghost'}>
+                <Image
+                  src="/icons/buttons/google-play.svg"
+                  width={132}
+                  height={36}
+                  alt="playstore"
+                />
+              </Button>
+            </motion.div>
+          </div>
+          {/* Image */}
+          <motion.div
+            variants={fadeIn('up', 0.6)}
+            initial="hidden"
+            whileInView={'show'}
+            viewport={{
+              once: false,
+              amount: 0.6,
+            }}
+            className="relative w-full h-full max-h-[50vh] md:max-w-[70vw] xl:max-w-[860px] xl:max-h-[542px] xl:absolute xl:-right-[100px] min-[1680px]:right-[120px] xl:top-48"
+          >
+            <Image
+              src={'/images/hero/car.svg'}
+              fill
+              alt="car image"
+              style={{ objectFit: 'cover' }}
+              priority
+            />
+          </motion.div>
         </div>
+      </div>
+
+      {searchActive ? (
+        <motion.div
+          initial={{ y: '-100%' }}
+          animate={{ y: 0 }}
+          transition={{ ease: easeInOut }}
+          className="fixed top-[80px] z-10 w-full max-w-[1920px]"
+        >
+          <Search />
+        </motion.div>
       ) : (
         <div className="-mt-12 w-full max-w-[1300px] mx-auto">
-          <Search />
+          <motion.div
+            variants={fadeIn('up', 0.8)}
+            initial="hidden"
+            whileInView={'show'}
+            viewport={{
+              once: false,
+              amount: 0.2,
+            }}
+          >
+            <Search />
+          </motion.div>
         </div>
       )}
     </section>
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import LocationSelection from './LocationSelection';
 import DateSelection from './DateSelection';
 import HoursSelection from './HoursSelection';
+import { Button } from './ui/button';
 
 export default function Search() {
   const { searchActive } = useSearchContext();
@@ -15,14 +16,20 @@ export default function Search() {
       className={cn(
         'hidden xl:block w-full relative shadow-lg bg-slate-800',
         searchActive
-          ? 'rounded-none xl:h-[80px]'
+          ? 'rounded-none xl:h-[85px] px-10 py-2'
           : 'rounded-[20px] py-6 xl:pr-4 xl:h-[98px]',
       )}
     >
-      <div className="xl:h-full flex items-center px-6 xl:px-0">
+      <div className={cn('flex h-full', searchActive && 'container mx-auto')}>
         <LocationSelection />
         <DateSelection />
         <HoursSelection />
+
+        <div className="xl:h-full w-36 px-6 xl:px-0">
+          <Button className={cn('h-full w-full text-lg text-white')}>
+            찾기
+          </Button>
+        </div>
       </div>
     </div>
   );
